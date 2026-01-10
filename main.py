4b008208292f44d8d26ff01063387f7fa453eb69
TJ Lamkin <tj@lamchen.net>
2026-01-09 16:11:27 -0800

    Feat: Basic AI prompt and response, with option for verbose output.

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from ai-agent!")
+import argparse
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+load_dotenv()
+
+try:
+    api_key = os.environ.get("GEMINI_API_KEY")
+except RuntimeError:
+    print("Please provide a valid Gemini API Key")
+
+parser = argparse.ArgumentParser(description="Prompt for Gemini AI 2.5-flash model.")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+client = genai.Client(api_key=api_key)
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+def main():
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=messages    
+        )
+    if response.usage_metadata != None:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+    else:
+        raise RuntimeError("API request has failed, please retry.")
+
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")    
+    print(response.text)
 
 if __name__ == "__main__":
     main()
